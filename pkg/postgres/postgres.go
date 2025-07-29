2429baa4a66429962fd90652e28c0ba67a9065fe
Mafit1 <mafit.tifam@gmail.com>
2025-07-29 18:23:49 +0300

    init

---
+++ pkg/postgres/postgres.go
@@ -0,0 +1,60 @@
+package postgres
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+const (
+	defaultConnectionTimeout  = time.Second
+	defaultConnectionAttempts = 10
+)
+
+type Postgres struct {
+	connectionTimeout  time.Duration
+	connectionAttempts int
+
+	Pool *pgxpool.Pool
+}
+
+func New(url string) (*Postgres, error) {
+	pg := &Postgres{
+		connectionTimeout:  defaultConnectionTimeout,
+		connectionAttempts: defaultConnectionAttempts,
+	}
+
+	poolConfig, err := pgxpool.ParseConfig(url)
+	if err != nil {
+		return nil, fmt.Errorf("parse config error: %w", err)
+	}
+
+	attempt := pg.connectionAttempts
+
+	for attempt > 0 {
+		pg.Pool, err = pgxpool.NewWithConfig(context.Background(), poolConfig)
+		if err != nil {
+			return nil, fmt.Errorf("pool creation error: %w", err)
+		}
+		if err = pg.Pool.Ping(context.Background()); err != nil {
+			break
+		}
+
+		attempt--
+		time.Sleep(pg.connectionTimeout)
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("postgres connection error, connection attempts = 0: %w", err)
+	}
+
+	return pg, nil
+}
+
+func (pg *Postgres) Close() {
+	if pg.Pool != nil {
+		pg.Pool.Close()
+	}
+}
