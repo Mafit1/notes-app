2ca88aebb5899edd5579cf90c6f1a6b5c3758311
Mafit1 <mafit.tifam@gmail.com>
2025-08-05 12:27:47 +0300

    config

---
+++ config/config.go
@@ -0,0 +1,51 @@
+package config
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type (
+	Config struct {
+		App      App      `yaml:"app"`
+		HTTP     HTTP     `yaml:"http"`
+		Postgres Postgres `yaml:"postgres"`
+		Log      Log      `yaml:"logger"`
+	}
+
+	App struct {
+		Name    string `env-required:"true" yaml:"name" env:"APP_NAME"`
+		Version string `env-required:"true" yaml:"version" env:"APP_VERSION"`
+	}
+
+	HTTP struct {
+		Port string `env-required:"true" yaml:"port" env:"SERVER_PORT"`
+	}
+
+	Postgres struct {
+		URL            string        `env-required:"true" yaml:"url" env:"POSTGRES_URL"`
+		ConnectionTime time.Duration `env-required:"true" yaml:"connect_timeout" env:"POSTGRES_CONNECT_TIMEOUT"`
+	}
+
+	Log struct {
+		Level string `env-required:"true" yaml:"level" env:"LOG_LEVEL"`
+	}
+)
+
+func New(configPath string) (*Config, error) {
+	cfg := &Config{}
+
+	err := cleanenv.ReadConfig(configPath, cfg)
+	if err != nil {
+		return nil, fmt.Errorf("config reading error: %w", err)
+	}
+
+	err = cleanenv.UpdateEnv(cfg)
+	if err != nil {
+		return nil, fmt.Errorf("congif updating error: %w", err)
+	}
+
+	return cfg, nil
+}
